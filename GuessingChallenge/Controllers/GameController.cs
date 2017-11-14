3d3b8e25f2d21153bfa222b07421fb8ac94a9120
Josh Lohman <jjosh9708@gmail.com>
2017-11-14 13:03:55 -0500

    Intial Commit

---
+++ GuessingChallenge/Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingChallenge.Models
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["answer"] = new Random().Next(1, 11);
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess) =>
+            guess == (int)Session["answer"];
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(GameModel model) //Latered refferred to the model state as a referrence to the requirments(max Characters etc..)
+        {
+            if (ModelState.IsValid)                             // Function of a ModelState talked about above
+            {
+                ViewBag.Win = GuessWasCorrect(model.Guess);     //View Bag is it a way to read what the user is guessing?
+            }
+            return View(model);
+        }
+    }
+}
---
+++ GuessingChallenge/Models/GameModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingChallenge.Models
+{
+    public class GameModel
+    {
+        [Required]
+        [Display(Name = "Player Name")]
+        [MaxLength(25, ErrorMessage ="Name must be less than 25 characters")]
+        public string PlayerName { get; set; }
+
+        [Range(1,10, ErrorMessage = "A valid response is a number 1-10") ]
+        public int Guess { get; set; }
+    }
+}
